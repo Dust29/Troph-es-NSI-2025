41c11898f3dadf74ea3b77e16ba38cad03bad7e0
Dust <150712174+Dust29@users.noreply.github.com>
2025-01-22 09:45:12 +0100

    Update Cat2.py

--- Code Source/Divers/Cat2.py
+++ Code Source/Divers/Cat2.py
@@ -10,7 +10,6 @@ from PySide6 import QtCore
 from PySide6.QtCore import QTimer
 
 #TODO Mettre l'elf au dessus du background
-#TODO le background avec un dictionnaire
 
 #Marche en PySide6
 
@@ -20,24 +19,22 @@ class Window(QWidget):
         super().__init__()
         self.setWindowTitle("PyQt5")
         self.resize(1920, 1080)
+        
         # Download a sprite sheet here:
         # https://plnkr.co/edit/zjYT0KTfj50MejT9?preview
-        self.sprite_sheet = QImage("F:\\DEV\\Trophèes '25\\Repo Github\\Code Source\\Divers\\ELF_RUN1_corrigé.gif")
+        self.sprite_sheet = QImage(".\\ELF_RUN1_corrigé.gif")
         self.sw = 64+16 #Original + buffer
         self.sh = 84
         self.frame_index = 0
         self.x = 0
         self.y = 0
         self.frames = []
-        self.dec=0
+        self.dec=256
         flag = True
         for i in range(2):
             for j in range(4):
                 self.frames.append(QPoint(j * self.sw, i * self.sh))
                 
-        
-        self.background()
-        
         self.delta_time = 0
         self.animation_time = 0
         self.animation_speed = 100
@@ -47,10 +44,43 @@ class Window(QWidget):
         
         self.elapsedTimer = QElapsedTimer()
         self.elapsedTimer.start()
-        self.timer.start(int(1000/60))   #Boucle pour l'animation
-        
-        
-        
+        self.timer.start(int(100/60))   #Boucle pour l'animation
+        self.a=True
+        self.LabelList = [[0] * 32 for i in range(32)] #Changer les dimensions si besoin
+        self.tileMap = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],                   
+                   [0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+            
+        self.background()
 #         self.timer = QTimer()
 #         self.timer.timeout.connect(self.animationLoop)
 #         self.elapsedTimer = QElapsedTimer()
@@ -71,25 +101,40 @@ class Window(QWidget):
         self.update()
 
     def paintEvent(self, event):
+        print(event)
         qp = QPainter(self)
         qp.drawImage(0+self.dec, 0, self.sprite_sheet, self.x, self.y, self.sw, self.sh)
+        if self.a:self.tileMap[0][0]=1
+        else:self.tileMap[0][0]=0
+        self.a=not self.a
+        
         
     def background(self):
         ListOfPath = [".\\tiles\\regularTile.png",".\\tiles\\bricksTile.png",]
-        LabelList = [[0] * 16 for i in range(32)] #Changer les dimensions si besoin
         
-        for i in range(int(512/16)):
-            for j in range(int(256/16)):
+        
+        #La carte est dans le mauvais sens (90° sur la gauche)
+        
+        
+        
+        
+        #TODO Ajouter dictionnaire + gestion par tilemap
+        
+        for i in range(int(512/16)): # Colonne 
+            for j in range(int(512/16)): #Ligne
             
                 self.labelImage = QLabel(self)
-                LabelList[i][j] = self.labelImage
-                pixmap2 = QPixmap(ListOfPath[i%2])
-                pixmap2 = pixmap2.scaled(16, 16, QtCore.Qt.KeepAspectRatio)
-                LabelList[i][j].setPixmap(pixmap2)
-                LabelList[i][j].move(16*i, 16*j)
-                print(f"Moved {i} to {16*i} {16*j}")
+                self.LabelList[j][i] = self.labelImage
+                pixmap2 = QPixmap(ListOfPath[self.tileMap[j][i]])
+                pixmap2.depth = 64
+                print(pixmap2)
+                #pixmap2 = pixmap2.scaled(16, 16, QtCore.Qt.KeepAspectRatio)
+                self.LabelList[j][i].setPixmap(pixmap2)
+                self.LabelList[j][i].move(16*i, 16*j)
+                #print(f"Moved {i} {j} to {16*i} {16*j}")
         
-        
+       
+
 #         self.labelImage = QLabel(self)
 #         LabelList.append(self.labelImage)
 #         pixmap = QPixmap("F:\\DEV\\Trophèes '25\\Repo Github\\Code Source\\Divers\\MAXI_BL2.GIF")
@@ -105,5 +150,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
